2f0784b0ab21c9d57de916e5415b8eb5683e41a2
BestMimic <123777254+BestMimic@users.noreply.github.com>
2023-05-06 05:20:35 -0500

    Puzzle 2 push
    The basics of puzzle 2 are ready. Grading is not implemented yet. The puzzle can not yet be played from the menu.

---
+++ src/scripts/shopping_bag_grab_zone.gd
@@ -0,0 +1,48 @@
+extends TextureRect
+
+var shopping_bag = preload("res://src/scenes/shopping_bag_piece.tscn")
+var root_scene
+var shopping_bag_icon_node
+var shopping_bag_instance
+var made_instance = false
+var area_entered = false
+
+# Called when the node enters the scene tree for the first time.
+# In this function root_scene is established as the node that contains the
+# hint box and the browser, logo_instance creates and instance of the
+# logo_piece scene, and logo_icon_node is established as the node
+# that shows the little logo icon in this scene.
+func _ready():
+	root_scene = get_node("/root/Puzzle2/Workspace")
+	shopping_bag_instance = shopping_bag.instantiate()
+	shopping_bag_icon_node = get_node("Small Shopping Bag")
+
+# Called when something in the Area2D emits input of some type.
+# If the input is a click and no logo has been made yet,
+# then create a logo and place it in the root_scene node as a
+# child. Then hide the Small Logo node. Otherwise if the click has been 
+# released on the area, check to see if an object is in the area and a logo
+# exists, then destroy the child and show the Small Logo node.
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	if Input.is_action_just_pressed("click"):
+		if made_instance == false:
+			made_instance = true
+			root_scene.add_child(shopping_bag_instance)
+			shopping_bag_icon_node.hide()
+			shopping_bag_instance.position = get_global_mouse_position()
+	if Input.is_action_just_released("click"):
+		if made_instance == true and area_entered == true:
+			made_instance = false
+			root_scene.remove_child(shopping_bag_instance)
+			shopping_bag_icon_node.show()
+			shopping_bag_instance.delete_instance()
+
+# Called if an object enters the area of Area2D.
+# If an object enters, switch area_entered to true.
+func _on_area_2d_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	area_entered = true
+	
+# Called if an object leaves the area of Area2D.
+# If an object leaves, switch area_entered to false.
+func _on_area_2d_area_shape_exited(area_rid, area, area_shape_index, local_shape_index):
+	area_entered = false
